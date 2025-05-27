e049cde5befc3fc942d002fe15f11464b05a02af
TayssirGh <tayssir.ghanmi@eniso.u-sousse.tn>
2025-05-27 14:14:19 +0100

    [TESTING] nginx

--- start-grafana.sh
+++ start-grafana.sh
@@ -2,9 +2,9 @@
 # Configure Grafana to use the Heroku-assigned port
 export GF_SERVER_HTTP_PORT=$PORT
 
+nginx
+
 # Start Grafana
 grafana server --homepath=/usr/share/grafana
 
-sed -i "s/\${PORT}/${PORT}/g" /etc/nginx/nginx.conf
 
-nginx -g 'daemon off;'
